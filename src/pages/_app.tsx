a2cf398ba794ba9e640abd1b601725c5fe417762
Edilson Borges <edilson.borges@agrodefesa.go.gov.br>
2021-04-13 23:48:46 -0300

    next auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
-import 'tailwindcss/tailwind.css';
+import React from 'react'
+import { Provider } from 'next-auth/client'
+import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
-import Head from 'next/head'
 import React from 'react'
+import { signIn, signOut, useSession } from 'next-auth/client'
+import Head from 'next/head'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Sidebar from '../components/Sidebar'
 import Body from '../components/Body'
 
 const Home: React.FC = () => {
+  const [session, loading] = useSession()
   return (
     <>
       <Head>
@@ -14,6 +16,22 @@ const Home: React.FC = () => {
       </Head>
       <Navbar />
       <div className="flex">
+        {!session && (
+          <>
+            Not signed in <br />
+            <button onClick={(): Promise<void> => signIn('auth0')}>
+              Sign in
+            </button>
+          </>
+        )}
+        {session && (
+          <>
+            Signed in as {session.user.email} <br />
+            <button onClick={(): Promise<void> => signOut('auth0')}>
+              Sign out
+            </button>
+          </>
+        )}
         <Sidebar />
         <Body />
       </div>
