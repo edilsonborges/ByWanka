1daf7e33453187c3c81e08caa6e5920fb1813ff9
Edilson Borges <edilson.borges@agrodefesa.go.gov.br>
2021-04-29 23:32:37 -0300

    validating signin visually

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -8,26 +8,45 @@ export default function signin(): JSX.Element {
     handleSubmit,
     formState: { errors }
   } = useForm()
+
+  const sendForm = (data) => {
+    console.log(data)
+  }
+
   return (
     <div className="flex flex-col bg-gray-100 content-area">
       <div className="container flex items-center justify-center flex-1 max-w-md px-2 mx-auto">
         <div className="w-full px-6 py-8 text-gray-700 bg-white rounded shadow-md">
           <h1 className="mb-8 text-3xl text-center">Login</h1>
-          <input
-            className="w-full p-3 mb-4 border border-gray-300 rounded"
-            type="text"
-            name="email"
-            placeholder="Email"
-          />
-          <input
-            className="w-full p-3 mb-4 border border-gray-300 rounded"
-            type="password"
-            name="password"
-            placeholder="Senha"
-          />
-          <button className="w-full p-3 mt-4 text-white bg-blue-400 rounded hover:bg-blue-500">
-            Entrar
-          </button>
+          <form onSubmit={handleSubmit(sendForm)}>
+            <input
+              className={
+                'w-full p-3 mb-4 rounded outline-none ' +
+                (errors.email
+                  ? 'border-2 border-red-400'
+                  : 'border border-gray-300')
+              }
+              type="text"
+              name="email"
+              placeholder="Email"
+              {...register('email', { required: true })}
+            />
+            <input
+              className={
+                'w-full p-3 mb-4 rounded outline-none ' +
+                (errors.password
+                  ? 'border-2 border-red-400'
+                  : 'border border-gray-300')
+              }
+              type="password"
+              name="password"
+              placeholder="Senha"
+              {...register('password', { required: true })}
+            />
+            <button className="w-full p-3 mt-4 text-white bg-blue-400 rounded hover:bg-blue-500">
+              Entrar
+            </button>
+          </form>
           <div className="mt-4 text-gray-700">
             Não tem uma conta?{' '}
             <Link href="/signup">
