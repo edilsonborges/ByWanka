65d96104b035b8d4c0b313ab4bd47d17d64b906e
Edilson Borges <edilson.borges@agrodefesa.go.gov.br>
2021-05-05 23:52:30 -0300

    find

--- src/pages/api/checkUser.tsx
+++ src/pages/api/checkUser.tsx
@@ -26,39 +26,11 @@ export default async (
   const { db } = await connect()
 
   if (req.method === 'POST') {
-    const query = {
-      email
-    }
-console.log(query);
 
-    const response = await db.collection('users').find(query)
-    console.log(response)
-    res.status(200).json(response)
+    const users = await db.collection('users').find({
+      email
+    }).forEach(response => {
+      res.status(200).json(response)
+    });
   }
 }
-
-
-
-// const MongoClient = require('mongodb').MongoClient;
-// const assert = require('assert');
-
-// /*
-//  * Requires the MongoDB Node.js Driver
-//  * https://mongodb.github.io/node-mongodb-native
-//  */
-
-// const filter = {
-//   'email': 'teste@teste.com'
-// };
-
-// MongoClient.connect(
-//   '',
-//   { useNewUrlParser: true, useUnifiedTopology: true },
-//   function(connectErr, client) {
-//     assert.equal(null, connectErr);
-//     const coll = client.db('bywanka').collection('users');
-//     coll.find(filter, (cmdErr, result) => {
-//       assert.equal(null, cmdErr);
-//     });
-//     client.close();
-//   });
