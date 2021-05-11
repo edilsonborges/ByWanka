83ec3eff0564fb7b9bee568141d3dda8ffcc62c3
Edilson Borges <edilson.borges@agrodefesa.go.gov.br>
2021-05-11 01:12:10 -0300

    bug fix

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -13,7 +13,7 @@ const [dropdownOpen, setDropdownOpen] = useState(false);
         <div className="flex items-center justify-center mt-8">
             <div className="flex items-center">
               <svg className="w-12 h-12" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M364.61 390.213C304.625 450.196 207.37 450.196 147.386 390.213C117.394 360.22 102.398 320.911 102.398 281.6C102.398 242.291 117.394 202.981 147.386 172.989C147.386 230.4 153.6 281.6 230.4 307.2C230.4 256 256 102.4 294.4 76.7999C320 128 334.618 142.997 364.608 172.989C394.601 202.981 409.597 242.291 409.597 281.6C409.597 320.911 394.601 360.22 364.61 390.213Z" fill="#4C51BF" stroke="#4C51BF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                  <path d="M364.61 390.213C304.625 450.196 207.37 450.196 147.386 390.213C117.394 360.22 102.398 320.911 102.398 281.6C102.398 242.291 117.394 202.981 147.386 172.989C147.386 230.4 153.6 281.6 230.4 307.2C230.4 256 256 102.4 294.4 76.7999C320 128 334.618 142.997 364.608 172.989C394.601 202.981 409.597 242.291 409.597 281.6C409.597 320.911 394.601 360.22 364.61 390.213Z" fill="#4C51BF" stroke="#4C51BF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
                   <path d="M201.694 387.105C231.686 417.098 280.312 417.098 310.305 387.105C325.301 372.109 332.8 352.456 332.8 332.8C332.8 313.144 325.301 293.491 310.305 278.495C295.309 263.498 288 256 275.2 230.4C256 243.2 243.201 320 243.201 345.6C201.694 345.6 179.2 332.8 179.2 332.8C179.2 352.456 186.698 372.109 201.694 387.105Z" fill="white"></path>
               </svg>
               <span className="mx-2 text-2xl font-semibold text-white">Dashboard</span>
@@ -23,9 +23,9 @@ const [dropdownOpen, setDropdownOpen] = useState(false);
             <a className="flex items-center px-6 py-2 mt-4 text-gray-100 bg-gray-700 bg-opacity-25" href="/">
               <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                   stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                     d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"></path>
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                     d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"></path>
               </svg>
               <span className="mx-3">Dashboard</span>
@@ -34,7 +34,7 @@ const [dropdownOpen, setDropdownOpen] = useState(false);
               href="/ui-elements">
               <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                   stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                     d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z">
                   </path>
               </svg>
@@ -44,7 +44,7 @@ const [dropdownOpen, setDropdownOpen] = useState(false);
               href="/tables">
               <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                   stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                     d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10">
                   </path>
               </svg>
@@ -54,7 +54,7 @@ const [dropdownOpen, setDropdownOpen] = useState(false);
               href="/forms">
               <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                   stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                     d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
                   </path>
               </svg>
@@ -68,8 +68,8 @@ const [dropdownOpen, setDropdownOpen] = useState(false);
               <button onClick={() =>
                   setSidebarOpen(true)} className="text-gray-500 focus:outline-none lg:hidden">
                   <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M4 6H20M4 12H20M4 18H11" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                        stroke-linejoin="round"></path>
+                    <path d="M4 6H20M4 12H20M4 18H11" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                        strokeLinejoin="round"></path>
                   </svg>
               </button>
               <div className="relative mx-4 lg:mx-0">
@@ -77,7 +77,7 @@ const [dropdownOpen, setDropdownOpen] = useState(false);
                     <svg className="w-5 h-5 text-gray-500" viewBox="0 0 24 24" fill="none">
                         <path
                           d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
-                          stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                          stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                         </path>
                     </svg>
                   </span>
@@ -92,7 +92,7 @@ const [dropdownOpen, setDropdownOpen] = useState(false);
                     <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                           d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
-                          stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                          stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                         </path>
                     </svg>
                   </button>
@@ -100,8 +100,7 @@ const [dropdownOpen, setDropdownOpen] = useState(false);
                     className="fixed inset-0 z-10 w-full h-full" style="display: none;">
                   </div>
                   <div x-show="notificationOpen" className="absolute right-0 z-10 mt-2 overflow-hidden bg-white rounded-lg shadow-xl w-80" style="width: 20rem; display: none;">
-                    <a href="#"
-                        className="flex items-center px-4 py-3 -mx-2 text-gray-600 hover:text-white hover:bg-indigo-600">
+                    <a href="#" className="flex items-center px-4 py-3 -mx-2 text-gray-600 hover:text-white hover:bg-indigo-600">
                         <img className="object-cover w-8 h-8 mx-1 rounded-full"
                           src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80"
                           alt="avatar">
@@ -110,8 +109,7 @@ const [dropdownOpen, setDropdownOpen] = useState(false);
                               className="font-bold text-indigo-400" href="#">Upload Image</span> artical . 2m
                         </p>
                     </a>
-                    <a href="#"
-                        className="flex items-center px-4 py-3 -mx-2 text-gray-600 hover:text-white hover:bg-indigo-600">
+                    <a href="#" className="flex items-center px-4 py-3 -mx-2 text-gray-600 hover:text-white hover:bg-indigo-600">
                         <img className="object-cover w-8 h-8 mx-1 rounded-full"
                           src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=634&amp;q=80"
                           alt="avatar">
@@ -119,8 +117,7 @@ const [dropdownOpen, setDropdownOpen] = useState(false);
                           <span className="font-bold" href="#">Slick Net</span> start following you . 45m
                         </p>
                     </a>
-                    <a href="#"
-                        className="flex items-center px-4 py-3 -mx-2 text-gray-600 hover:text-white hover:bg-indigo-600">
+                    <a href="#" className="flex items-center px-4 py-3 -mx-2 text-gray-600 hover:text-white hover:bg-indigo-600">
                         <img className="object-cover w-8 h-8 mx-1 rounded-full"
                           src="https://images.unsplash.com/photo-1450297350677-623de575f31c?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80"
                           alt="avatar">
@@ -164,6 +161,7 @@ const [dropdownOpen, setDropdownOpen] = useState(false);
               </div>
             </div>
         </header> */}
+
         {/* <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
             <div className="container px-6 py-8 mx-auto">
               <h3 className="text-3xl font-medium text-gray-700">Dashboard</h3>
@@ -228,10 +226,10 @@ const [dropdownOpen, setDropdownOpen] = useState(false);
                               <svg className="w-8 h-8 text-white" viewBox="0 0 28 28" fill="none"
                                 xmlns="http://www.w3.org/2000/svg">
                                 <path d="M6.99998 11.2H21L22.4 23.8H5.59998L6.99998 11.2Z" fill="currentColor"
-                                    stroke="currentColor" stroke-width="2" stroke-linejoin="round"></path>
+                                    stroke="currentColor" strokeWidth="2" strokeLinejoin="round"></path>
                                 <path
                                     d="M9.79999 8.4C9.79999 6.08041 11.6804 4.2 14 4.2C16.3196 4.2 18.2 6.08041 18.2 8.4V12.6C18.2 14.9197 16.3196 16.8 14 16.8C11.6804 16.8 9.79999 14.9197 9.79999 12.6V8.4Z"
-                                    stroke="currentColor" stroke-width="2"></path>
+                                    stroke="currentColor" strokeWidth="2"></path>
                               </svg>
                           </div>
                           <div className="mx-5">
